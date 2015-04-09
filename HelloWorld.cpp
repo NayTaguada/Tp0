99bb6f6eec47cd187e13f04575f064b92790d948
Nayla <naylalucia@gmail.com>
2015-04-09 12:01:17 -0300

    Mi primer comit
    AED - Nayla Taguada - K1091

---
+++ HelloWorld.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : HelloWorld.cpp
+// Author      : Nayla
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+//Nayla Taguada
+//Curso: k1091
+//AED - Turno: Noche
+//Fecja 05/04/15
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello Nayla!!!" << endl;
+	return 0;
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+
+
+
+TP0 - Hello World - AED
+
+
+Hellow Nayla!
+
+Process returned 0 <0x0> execution time : 2.001 s
+Press ane key to continue.
